a746ae620cea75db97e6b2a1e208540d224c8067
FANGPLAY SIRIRAK <fangplay_2014@hotmail.com>
2023-02-20 18:17:17 +0700

    add gamelist page

--- src/gamelist.js
+++ src/gamelist.js
@@ -1,5 +1,155 @@
-function GameList(){
+import React from 'react';
 
+function GameList() {
+    return (
+        <div class="container">
+            <h4 class="col-center display-4">Gamer History</h4>
+            <div class="row">
+                <div class="col-md-6">
+                    <h4 class="display-5">GEN 1 (2007-2008)</h4>
+                    <p>Starcraft</p>
+                    <p>Command & Conquer : Tiberian Sun & Firestorm</p>
+                </div>
+                <div class="col-md-6">
+                    <h4 class="display-5">GEN 2 (2008-2009)</h4>
+                    <p>DUNE 2000</p>
+                    <p>Command & Conquer : Red Alert 2 & Yuri's Revenge</p>
+                    <p>The Sims 1</p>
+                    <p>Audition</p>
+                    <p>Tales Runner(First Game Online for myself)</p>
+                    <p>RayCity</p>
+                </div>
+                <div class="col-md-6">
+                    <h4 class="display-5">GEN 3 (2009-2011)</h4>
+                    <p>Angry Birds</p>
+                    <p>Command & Conquer : Renegade</p>
+                    <p>Command & Conquer</p>
+                    <p>Command & Conquer : Red Alert 1</p>
+                    <p>Zuma's Collection</p>
+                    <p>Plants VS Zombies</p>
+                    <p>Need For Speed Most Wanted</p>
+                    <p>Need For Speed Underground 1</p>
+                    <p>Need For Speed Underground 2</p>
+                </div>
+                <div class="col-md-6">
+                    <h4 class="display-5">GEN 4&4.5 (2012-2014)</h4>
+                    <p>Renegade X</p>
+                    <p>Simcity(2013)</p>
+                    <p>Command & Conquer 3 : Tiberian Wars</p>
+                    <p>Command & Conquer 3 : Kane's Wrath</p>
+                    <p>Command & Conquer : Red Alert 3</p>
+                    <p>Command & Conquer : Red Alert 3 Uprissing</p>
+                    <p>Command & Conquer 4 : Tiberian Twilights</p>
+                    <p>LINE let's get rich</p>
+                    <p>Resident Evil 6</p>
+                    <p>Need For Speed Carbon</p>
+                    <p>Need For Speed Prostreet</p>
+                    <p>Need For Speed Undercover</p>
+                    <p>Need For Speed Shift</p>
+                    <p>Mirrors Edge</p>
+                    <p>SimCity 4</p>
+                    <p>Fruit Ninja</p>
+                </div>
+                <div class="col-md-6">
+                    <h4 class="display-5">GEN 5 (2015-2017)</h4>
+                    <p>Agar.io</p>
+                    <p>Cities Skylines</p>
+                    <p>Counter Strike : Global Offensive(STEAM)</p>
+                    <p>Mirrors Edge : Catalyst</p>
+                    <p>The Sims 2</p>
+                    <p>The Sims 3</p>
+                    <p>The Sims 4</p>
+                    <p>Mobius Final Fantasy</p>
+                    <p>Bioshock 1</p>
+                    <p>Bioshock 2</p>
+                    <p>Bioshock Infinite</p>
+                    <p>Fallout New Vegas</p>
+                    <p>Fallout 3</p>
+                    <p>Fallout 4</p>
+                    <p>Fallout Shelter</p>
+                    <p>Slither.io</p>
+                </div>
+                <div class="col-md-6">
+                    <h4 class="display-5">SPECTRUM & NEOSPECTRUM GEN (2017-PRESENT)</h4>
+                    <p>PUBG LITE(Stopped Support)(PC)</p>
+                    <p>HACKNET(PC)</p>
+                    <p>CALLS OF DUTY MOBILE(Mobile)</p>
+                    <p>APEX LEGENDS(STEAM+ORIGIN)</p>
+                    <p>Cyber Hunter(PC & Mobile)</p>
+                    <p>HONKAI IMPACT 3RD(PC & Mobile)</p>
+                    <p>GENSHIN IMPACT(Multi-Platforms)</p>
+                    <p>UNO(Multi-Platforms)</p>
+                    <p>EVE ONLINE(PC)</p>
+                    <p>EVE ECHOES(Mobile)</p>
+                    <p>Creative Destruction(Stopped Support)(Multi-Platforms)</p>
+                    <p>Angel Legion(PC & Mobile)</p>
+                    <p>OVERWATCH(BATTLE.NET)</p>
+                    <p>PALADINS(STEAM)</p>
+                    <p>PUBG MOBILE(Mobile)</p>
+                    <p>Super Mecha Champions(SMC)(Multi-Platforms)</p>
+                    <p>Life After(Multi-Platforms)</p>
+                    <p>Fallout Shelter Online(Mobile)</p>
+                    <p>ORWELL</p>
+                    <p>Identity V(Multi-Platforms)</p>
+                    <p>Darkness Rises(Mobile)</p>
+                    <p>Audition Mobile(Mobile)</p>
+                    <p>PANGYA Mobile(Stopped Support)(Mobile)</p>
+                    <p>AxE : Alliance vs Empire(Mobile)</p>
+                    <p>Disorder(Mobile)</p>
+                    <p>League Of Legend : Wild Raft(Mobile)</p>
+                    <p>MARVEL Super War(Mobile)</p>
+                    <p>Among Us(PC & Mobile)</p>
+                    <p>Dead Or Alive Xtreme Venus Vacation(PC)</p>
+                    <p>Tom and Jerry : Chase(Mobile)</p>
+                    <p>Date A Live : Spirit Pledge</p>
+                    <p>Illusion Connect(Mobile)</p>
+                    <p>Arknights(Mobile)</p>
+                    <p>Azur Lane(Mobile)</p>
+                    <p>Goddess Of Genesis(Stopped Support ON SEA Server)(Mobile)</p>
+                    <p>Game Of Dice(Mobile)</p>
+                    <p>Dragon Raja(Mobile)</p>
+                    <p>Strike Royale(Mobile)</p>
+                    <p>CRISIS : S(Mobile)</p>
+                    <p>Onmyoji Arena(Mobile)</p>
+                    <p>Ultimate Tennis(Mobile)</p>
+                    <p>Extreme Football(Mobile)</p>
+                    <p>Need For Speed No Limits(Mobile)</p>
+                    <p>House Flipper(PC)</p>
+                    <p>Tomb Raider(2013)(PC)</p>
+                    <p>Rise Of The Tomb Raider(2015)(PC)</p>
+                    <p>Prop And Seek(PC)</p>
+                    <p>Crab Game(PC)</p>
+                    <p>Shadow Verse(Multi-Platforms)</p>
+                    <p>Rouge Company(PC)</p>
+                    <p>Bright Memory(PC)</p>
+                    <p>Just Cause 3(PC)</p>
+                    <p>Just Cause 2(PC)</p>
+                    <p>Chimera Land(Multi-Platforms)</p>
+                    <p>Need For Speed 2015(STEAM+ORIGIN)</p>
+                    <p>PC Building Simulator(PC)</p>
+                    <p>New State Mobile(Mobile)</p>
+                    <p>APEX Legends Mobile(Android+IOS)</p>
+                    <p>YU-GI-OH Master Duel(Multi-Platforms)</p>
+                    <p>Watch Dogs(PC)</p>
+                    <p>Assassins Creed Unity(PC,Ubisoft Connect)</p>
+                    <p>Lost Light(Multi-Platforms)</p>
+                    <p>Naraka Breakpoint(PC)</p>
+                    <p>Naraka Breakpoint Mobile(Mobile)</p>
+                    <p>Tom Clancy's : Rainbow Six Siege(PC)</p>
+                    <p>Shadow of the Tomb Raider(2018)(PC)</p>
+                    <p>Dead by Daylight Mobile(Mobile)</p>
+                    <p>Dead by Daylight(PC)</p>
+                    <p>Watch Dogs 2(PC)</p>
+                    <p>Watch Dogs Legion(PC)</p>
+                    <p>Valorant(PC)</p>
+                    <p>Valorant Mobile(Mobile)</p>
+                    <p>Battlefield Mobile(Mobile)</p>
+                    <p>Punk Royale 2052(Mobile)</p>
+                    <p>Honkai Star Rail(Multi-Platforms)</p>
+                    <p>NIKKE : The Goddess of Victory(Mobile)</p>
+                </div>
+            </div>
+        </div>
+            );
 }
-
-export default GameList;
+ export default GameList;
